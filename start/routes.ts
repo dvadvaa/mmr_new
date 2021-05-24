f85e86378f5bc4131b18c4e00c3098173cb0014a
Nikita <2chruu@gmail.com>
2021-05-24 23:40:49 +0300

    update

--- start/routes.ts
+++ start/routes.ts
@@ -39,8 +39,9 @@ Route.group(() => {
   Route.post('/release/new', 'DashboardController.createRelease')
 }).prefix('/api/user').middleware('auth')
 
-Route.get('/', async ({ view }) => {
-  return view.render('index')
+Route.get('/', async ({ view, bouncer }) => {
+  await view.render('index')
+  await bouncer.authorize('index')
 }).as('index')
 
 Route.get('/success', async ({ view }) => {
